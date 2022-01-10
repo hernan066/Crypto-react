37b592d1335ca4ca8509f5feff939d441634adc3
hernangit config --global user.name John <hernan0066@gmail.com>
2022-01-10 12:43:55 -0300

    Responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,57 @@
-import React from 'react';
-import { Button, Menu, Typography, Avatar} from 'antd';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
-import icon from '../image/cryptocurrency.png'
+import icon from '../image/cryptocurrency.png';
 
 const Navbar = () => {
-    return (
-        <div className='nav-container'>
-            <div className="logo-container">
-                <Avatar src={icon} size='large' />
-                <Typography.Title level={2} className='logo'>
-                     <Link to='/'> Cryptoreact </Link> 
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
 
-                </Typography.Title>
-                {/* <button className="menu-control-container">
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
 
-                </button> */}
+    window.addEventListener('resize', handleResize);
 
-            </div>
+    handleResize();
 
-            <Menu theme='dark'>
-                <Menu.Item key={1} icon={<HomeOutlined />}>
-                    <Link to='/'> Home </Link>
-                </Menu.Item>
-                <Menu.Item key={2} icon={<FundOutlined />}>
-                    <Link to='/crypocurrencies'> Crypocurrencies </Link>
-                </Menu.Item>
-                <Menu.Item key={3} icon={<MoneyCollectOutlined />}>
-                    <Link to='/exchanges'> Exchanges </Link>
-                </Menu.Item>
-                <Menu.Item key={4} icon={<BulbOutlined />}>
-                    <Link to='/news'> News </Link>
-                </Menu.Item>
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-            </Menu>
-            
-        </div>
-    )
-}
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
-export default Navbar
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo"><Link to="/">Cryptoverse</Link></Typography.Title>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+      </div>
+      {activeMenu && (
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
